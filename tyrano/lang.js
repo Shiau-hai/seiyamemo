b5bdf32dde0b3127d4da62588a8431afd5d8dcd0
ternsadmin <shinyarou233@outlook.com>
2018-04-07 21:56:28 +0900

    first commit

---
+++ tyrano/lang.js
@@ -0,0 +1,33 @@
+window.tyrano_lang = {
+
+    word : {
+        
+        "go_title":"将返回主菜单，确定吗？",
+        "exit_game":"是否退出游戏并关闭当前页面？",
+        "not_saved":"这里还没有存档哦？",
+        "tag":"标签",
+        "not_exists":"不存在",
+        "error":"程序发生错误，请联系作者。",
+        "label":"标签",
+        "label_double":"标签重复了",
+        "error_occurred":"程序发生错误，请联系作者。"
+    },
+
+    novel : {
+        "file_menu_button_save":"menu_button_save.gif",
+        "file_menu_button_load":"menu_button_load.gif",
+        "file_menu_button_message_close":"menu_message_close.gif",
+        "file_menu_button_skip":"menu_button_skip.gif",
+        "file_menu_button_title":"menu_button_title.gif",
+        "file_menu_button_close":"menu_button_close.png",
+        "file_menu_bg":"menu_bg.jpg",
+        "file_save_bg":"menu_save_bg.jpg",
+        "file_load_bg":"menu_load_bg.jpg",
+        "file_button_menu":"button_menu.png",
+        "error_occurred":"error occurred"
+    
+        
+    }
+
+};
+
